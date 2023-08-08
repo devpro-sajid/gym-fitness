1777b4cf079ab7ea5697f6c1e0aef0ea02c4a511
devpro-sajid <sfsajidforclash01@gmail.com>
2023-08-08 11:04:03 +0600

    footer complete

--- src/scenes/Footer/index.tsx
+++ src/scenes/Footer/index.tsx
@@ -1,7 +1,8 @@
 import Logo from "@/assets/Logo.png";
 
 const Footer = () => {
-  return (
+  return ( 
+    
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
